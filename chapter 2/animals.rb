70b27c2c0532a48ca58b9f82d1613013c55c3bfd
Pavel Mikhaylov <mikhailov.sztu@gmail.com>
2021-04-07 17:15:18 +0300

    done second chapter

---
+++ chapter 2/animals.rb
@@ -0,0 +1,57 @@
+class Bird
+  def talk(name)
+    puts "#{name} says Chirp! Chirp!"
+  end
+  def move(name, destination)
+    puts "#{name} flies to the #{destination}."
+  end
+end
+
+class Dog
+  attr_reader :name, :age
+
+  def name=(value)
+    if value == ""
+      raise "Name can't be blank!"
+    end
+    @name = value
+  end
+
+  def age=(value)
+    if value < 0
+      raise "An age of #{value} isn't valid!"
+    end
+      @age = value
+  end
+
+  def report_age
+    puts "#{@name} is #{@age} years old."
+  end
+
+  def talk
+    puts "#{@name} says Bark!"
+  end
+
+  def move(destination)
+    puts "#{@name} runs to the #{destination}"
+  end
+end
+
+class Cat
+  def talk(name)
+    puts "#{name} says Meow!"
+  end
+  def move(name, destination)
+    puts "#{name} runs to the #{destination}"
+  end
+end
+
+bird = Bird.new
+dog = Dog.new
+dog.name = "Fido"
+cat = Cat.new
+
+bird.move('Birdi', 'tree')
+dog.talk
+bird.talk('Birdi')
+cat.move('Silba', 'house')
