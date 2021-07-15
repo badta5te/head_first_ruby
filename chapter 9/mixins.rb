363476e1187bd15064dea1e6bbbd1f9f20a44bda
Pavel Mikhaylov <mikhailov.sztu@gmail.com>
2021-07-15 18:23:00 +0300

    done chapter 9

---
+++ chapter 9/mixins.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+module AcceptsComments
+  def comments
+    @comments ||= []
+  end
+
+  def add_comment(comment)
+    comments << comment
+  end
+end
+
+class Clip
+  def play
+    puts "Playing #{object_id}..."
+  end
+end
+
+class Video < Clip
+  include AcceptsComments
+  attr_accessor :resolution
+end
+
+class Song < Clip
+  include AcceptsComments
+  attr_accessor :beats_per_minute
+end
+
+class Photo
+  include AcceptsComments
+  def show
+    puts "Displaying #{object_id}..."
+  end
+end
+
+video = Video.new
+video.add_comment('Cool slow motion effect!')
+video.add_comment('Weird effect!')
+song = Song.new
+song.add_comment('Awesome beat!')
+
+p video.comments, song.comments
+video.play
+song.play
+
+photo = Photo.new
+photo.add_comment('Beautiful colors.')
+p photo.comments
+photo.show
